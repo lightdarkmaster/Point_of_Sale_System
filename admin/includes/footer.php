21438afb87bc022414caf119842615cba89bb7d0
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-12-14 21:44:01 +0800

    Make javascript CDN remove local js script

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -42,7 +42,6 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="assets/js/myscript.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -16,7 +16,6 @@ require 'authentication.php';
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
         <link href="assets/css/mystyle.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <script src="assets/js/bootstrap.bundle.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     </head>
     <body class="sb-nav-fixed"> 
