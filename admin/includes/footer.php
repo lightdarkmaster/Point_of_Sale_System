676c0f2fe9698f747601a38ca4c6f930fbb170f4
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-12-14 21:02:19 +0800

    Add bootstrap for offline

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -42,6 +42,7 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="assets/js/myscript.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
